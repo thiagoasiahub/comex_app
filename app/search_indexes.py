d5711515737421a82b818806bbc946621c5d08b1
Thiago R.  da Costa <thiagor.dacosta@Mac-mini.local>
2020-09-20 17:52:20 -0300

    colorschema_newcolumns

--- app/search_indexes.py
+++ app/search_indexes.py
@@ -6,8 +6,11 @@ class CustomerIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     incoterm = indexes.CharField(model_attr='incoterm')
     descricao_do_produto = indexes.CharField(model_attr='descricao_do_produto')
+    valor_un_prod_dolar = indexes.CharField(model_attr='valor_un_prod_dolar')
     qtd_comercial_field = indexes.CharField(model_attr='qtd_comercial_field')
+    anomes = indexes.CharField(model_attr='anomes')
     cod_ncm = indexes.CharField(model_attr='cod_ncm')
+    descricao_do_codigo_ncm = indexes.CharField(model_attr='descricao_do_codigo_ncm')
 
     
     def get_model(self):
